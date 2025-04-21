e0c0038206d9810ecbfaa01a3c0c18e4906901f0
Harrison Goldstein <hgoldstein95@gmail.com>
2025-04-21 16:02:03 -0400

    Refactor

--- RayTracer/Entities/Sphere.lean
+++ RayTracer/Entities/Sphere.lean
@@ -12,33 +12,32 @@ structure Args where
   radius : Float
   material : Material
 
-def mk (s : Args) : Entity :=
-  λ (r : Ray) tRange => do
-    let oc : Vec3 := s.center - r.origin
-    let a := r.direction.lengthSquared
-    let h := r.direction ⬝ oc
-    let c := oc.lengthSquared - s.radius * s.radius
+def mk (s : Args) : Entity := λ (r : Ray) tRange => do
+  let oc : Vec3 := s.center - r.origin
+  let a := r.direction.lengthSquared
+  let h := r.direction ⬝ oc
+  let c := oc.lengthSquared - s.radius * s.radius
 
-    let discriminant := h * h - a * c
-    if discriminant < 0 then none
+  let discriminant := h * h - a * c
+  if discriminant < 0 then none
 
-    let sqrtd := discriminant.sqrt
-    let root₁ := (h - sqrtd) / a
-    let root₂ := (h + sqrtd) / a
+  let sqrtd := discriminant.sqrt
+  let root₁ := (h - sqrtd) / a
+  let root₂ := (h + sqrtd) / a
 
-    let root : Option Float :=
-      if tRange.surrounds root₁ then
-        root₁
-      else if tRange.surrounds root₂ then
-        root₂
-      else
-        none
-
-    if let some t := root then
-      let point := r.at t
-      let outwardNormal := (point - s.center) / s.radius
-      Collision.mkWithOutwardNormal r t point outwardNormal s.material
+  let root : Option Float :=
+    if tRange.surrounds root₁ then
+      root₁
+    else if tRange.surrounds root₂ then
+      root₂
     else
       none
+
+  if let some t := root then
+    let point := r.at t
+    let outwardNormal := (point - s.center) / s.radius
+    Collision.mkWithOutwardNormal r t point outwardNormal s.material
+  else
+    none
 
 end Sphere
