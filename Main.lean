b7c7b4e458bb9b713d1b8192329a689a33ccd55d
Harrison Goldstein <hgoldstein95@gmail.com>
2025-04-22 13:22:12 -0400

    Messing around in Main

--- Main.lean
+++ Main.lean
@@ -5,7 +5,7 @@ import RayTracer.Basic
 open Lean (ToJson FromJson Json)
 
 def main : IO Unit := do
-  let config := {
+  let config : CameraConfig := {
     aspectRatio := 16.0 / 9.0,
     imageWidth := 400,
     maxRayDepth := 50,
@@ -13,11 +13,11 @@ def main : IO Unit := do
     samplesPerPixel := 100,
   }
 
-  let fileName := "scenes/four-spheres.json"
-  let .ok json := Json.parse (← IO.FS.readFile fileName)
-    | IO.throwServerError s!"Failed to parse {fileName}"
+  let scene := include_str "scenes" / "four-spheres.json"
+  let .ok json := Json.parse scene
+    | IO.throwServerError s!"Failed to parse"
   let .ok world := FromJson.fromJson? json
-    | IO.throwServerError s!"Failed to deserialize {fileName}"
+    | IO.throwServerError s!"Failed to deserialize"
 
   let camera ← Camera.init config
   IO.println <| PPM.display (← camera.render world)
