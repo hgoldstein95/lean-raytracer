a3f255b9f503e8bbef6498afece8d4e6bac4dec3
Harrison Goldstein <hgoldstein95@gmail.com>
2025-04-22 13:17:42 -0400

    Rearranged

--- RayTracer/FastRandom.lean
+++ RayTracer/FastRandom.lean
@@ -1,24 +1,26 @@
 namespace FastRandom
 
-structure Seed where
+structure GenSt where
   s1 : UInt64
   s2 : UInt64
 
-def mkSeed (s : UInt64 := 0) : Seed :=
+instance : Inhabited GenSt where
+  default := ⟨0, 0⟩
+
+instance : Repr GenSt where
+  reprPrec | ⟨s1, s2⟩, _ => Std.Format.bracket "⟨" (repr s1 ++ ", " ++ repr s2) "⟩"
+
+namespace GenSt
+
+def init (s : UInt64 := 0) : GenSt :=
   let q  := s / 2147483562
   let s1 := s % 2147483562
   let s2 := q % 2147483398
   ⟨s1 + 1, s2 + 1⟩
 
-instance : Inhabited Seed where
-  default := ⟨0, 0⟩
+def maxValue : Float := 2147483562
 
-def seedMax : Float := 2147483562
-
-instance : Repr Seed where
-  reprPrec | ⟨s1, s2⟩, _ => Std.Format.bracket "⟨" (repr s1 ++ ", " ++ repr s2) "⟩"
-
-def seedNext : Seed → UInt64 × Seed
+def next : GenSt → UInt64 × GenSt
   | ⟨s1, s2⟩ =>
     let k    := s1 / 53668
     let s1'  := 40014 * (s1 - k * 53668) - k * 12211
@@ -30,25 +32,20 @@ def seedNext : Seed → UInt64 × Seed
     let z'   := if z < 1 then (z + 2147483562) else z % 2147483562
     (z', ⟨s1'', s2''⟩)
 
-def seedSplit : Seed → Seed × Seed
-  | g@⟨s1, s2⟩ =>
-    let newS1  := if s1 = 2147483562 then 1 else s1 + 1
-    let newS2  := if s2 = 1          then 2147483398 else s2 - 1
-    let newG   := (seedNext g).2
-    let leftG  := Seed.mk newS1 newG.2
-    let rightG := Seed.mk newG.1 newS2
-    (leftG, rightG)
+end GenSt
 
 end FastRandom
 
-initialize IO.fastRandomRef : IO.Ref FastRandom.Seed ←
+open FastRandom
+
+initialize IO.fastRandomRef : IO.Ref GenSt ←
   let seed := ByteArray.toUInt64LE! (← IO.getRandomBytes 8)
-  IO.mkRef (FastRandom.mkSeed seed)
+  IO.mkRef (GenSt.init seed)
 
 -- In range [0, 1)
 def IO.randFloat : IO Float := do
-  let n ← IO.fastRandomRef.modifyGet FastRandom.seedNext
-  return (n.toFloat - 1.0) / FastRandom.seedMax
+  let n ← IO.fastRandomRef.modifyGet GenSt.next
+  return (n.toFloat - 1.0) / GenSt.maxValue
 
 def IO.randFloatInRange (min max : Float) : IO Float := do
   let f ← IO.randFloat
